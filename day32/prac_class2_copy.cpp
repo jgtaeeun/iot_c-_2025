98369ba28abd0a0388a09a153fa4a34cd248baf5
jgtaeeun <jas49258667@gmail.com>
2025-03-20 08:58:35 +0900

    32일차(3/19)클래스연습

--- day32/prac_class2_copy.cpp
+++ day32/prac_class2_copy.cpp
@@ -1,34 +1,37 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-
-class Printer {
-
-private:
-	char* str;
-public:
-	void setString(const char* s) {
-		str = new char[strlen(s) + 1];
-
-		strcpy(str, s);
-	}
-
-	void showString() {
-		std::cout << str << std::endl;
-	}
-
-	~Printer() {
-
-		delete[] str;
-	}
-
-};
-int main(void) {
-	Printer p;
-	p.setString("hello world!");
-	p.showString();
-
-	Printer p2;
-	p2.setString("I love c++");
-	p2.showString();
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//
+//class Printer {
+//
+//private:
+//	char* str;
+//
+//public:
+//	void setString(const char* s) {
+//		//동적할당이 필요한 이유
+//		/*초기화되지 않은 포인터 사용: char* 포인터는 메모리 주소를 저장하는 변수이므로, 초기화되지 않은 상태로 사용할 경우 쓰레기 값을 참조할 수 있습니다. */
+//		str = new char[strlen(s) + 1];  
+//
+//		strcpy(str, s);
+//	}
+//
+//	void showString() {
+//		std::cout << str << std::endl;
+//	}
+//
+//	~Printer() {
+//
+//		delete[] str;
+//	}
+//
+//};
+//int main(void) {
+//	Printer p;
+//	p.setString("hello world!");
+//	p.showString();
+//
+//	Printer p2;
+//	p2.setString("I love c++");
+//	p2.showString();
+//	return 0;
+//}
